f1357914fa25c4faea812fbd03688dd188ff828d
Jason Holloway <jason.holloway@sorted.com>
2021-03-25 00:01:50 +0000

    windows keys sent to handler

--- src/main.rs
+++ src/main.rs
@@ -18,18 +18,19 @@ mod unix;
 mod common;
 mod null;
 
+fn create_handler<T>() -> Handler<T> {
+    Handler {
+        count: 0,
+        keys: Bitmap::new(),
+        buff: VecDeque::new(),
+        mode: Mode::Root
+    }
+}
 
 pub fn main() {
-		let mut handler = Handler {
-				count: 0,
-				keys: Bitmap::new(),
-				buff: VecDeque::new(),
-				mode: Mode::Root
-		};
-    
     cfg_if::cfg_if! {
         if #[cfg(windows)] {
-            windows::run(&mut state, handle).unwrap();
+            windows::run(create_handler).unwrap();
         } else if #[cfg(unix)] {
             unix::run(&mut handler).unwrap();
         } else {
@@ -47,15 +48,15 @@ use std::collections::vec_deque::*;
 
 
 trait Sink<T> {
-		fn emit(item: &T);
+    fn emit(item: &T);
 }
 
 
 pub struct Handler<TRaw> {
     count: u32,
     keys: Bitmap<U1024>,
-		buff: VecDeque<Update<TRaw>>,
-		mode: Mode
+    buff: VecDeque<Update<TRaw>>,
+    mode: Mode
 }
 
 
@@ -64,124 +65,125 @@ pub struct Handler<TRaw> {
 
 impl<TRaw> Handler<TRaw>
 where
-		TRaw: Debug
+    TRaw: Debug
 {
 
-		fn handle(&mut self, update: Update<TRaw>) -> (NextDue, Drain<Update<TRaw>>)
-		{
-				self.count += 1;
-				// println!("{} {:?}", self.count, update);
-				println!("{:?}", self.keys.into_iter().collect::<Vec<usize>>());
+    fn handle(&mut self, update: Update<TRaw>) -> (NextDue, Drain<Update<TRaw>>)
+    {
+        self.count += 1;
+        // println!("{} {:?}", self.count, update);
 
-				if let Key(code, movement, _) = update {
-						match movement {
-								Up => self.keys.set(code as usize, false),
-								Down => self.keys.set(code as usize, true),
-						};
-				}
+        if let Key(code, movement, _) = update {
+            match movement {
+                Up => self.keys.set(code as usize, false),
+                Down => self.keys.set(code as usize, true),
+            };
+        }
+
+        println!("{:?}", self.keys.into_iter().collect::<Vec<usize>>());
 
 
-				use Mode::*;
-				let current_mode = self.mode;
+        use Mode::*;
+        let current_mode = self.mode;
 
-				match (current_mode, &update) {
-						(AltShiftJ, Key(36, Up, _)) => {
-								println!("~DOWN!")
-						},
-						(AltShiftK, Key(37, Up, _)) => {
-								self.buff.push_back(Key(103, Up, None));
-								println!("~UP!")
-						},
-						(AltShiftSpace, Key(57, Up, _)) => {
-								self.buff.push_back(Key(28, Up, None));
-								println!("~RETURN!")
-						},
+        match (current_mode, &update) {
+            (AltShiftJ, Key(36, Up, _)) => {
+                println!("~DOWN!")
+            },
+            (AltShiftK, Key(37, Up, _)) => {
+                self.buff.push_back(Key(103, Up, None));
+                println!("~UP!")
+            },
+            (AltShiftSpace, Key(57, Up, _)) => {
+                self.buff.push_back(Key(28, Up, None));
+                println!("~RETURN!")
+            },
 
-						_ => {}
-				}
+            _ => {}
+        }
 
-				let next_mode = match (current_mode, &update) {
-						(Root, Key(42, Down, _)) => Shift,
-						(Root, Key(56, Down, _)) => Alt,
+        let next_mode = match (current_mode, &update) {
+            (Root, Key(42, Down, _)) => Shift,
+            (Root, Key(56, Down, _)) => Alt,
 
-						(Shift, Key(42, Up, _)) => Root,
-						(Shift, Key(56, Down, _)) => AltShift,
+            (Shift, Key(42, Up, _)) => Root,
+            (Shift, Key(56, Down, _)) => AltShift,
 
-						(Alt, Key(56, Up, _)) => Root,
-						(Alt, Key(42, Down, _)) => AltShift,
+            (Alt, Key(56, Up, _)) => Root,
+            (Alt, Key(42, Down, _)) => AltShift,
 
-						(AltShift, Key(42, Up, _)) => Alt,
-						(AltShift, Key(56, Up, _)) => Shift,
-						(AltShift, Key(36, Down, _)) => AltShiftJ,
-						(AltShift, Key(37, Down, _)) => AltShiftK,
-						(AltShift, Key(57, Down, _)) => AltShiftSpace,
+            (AltShift, Key(42, Up, _)) => Alt,
+            (AltShift, Key(56, Up, _)) => Shift,
+            (AltShift, Key(36, Down, _)) => AltShiftJ,
+            (AltShift, Key(37, Down, _)) => AltShiftK,
+            (AltShift, Key(57, Down, _)) => AltShiftSpace,
 
-						(AltShiftSpace, Key(42, Up, _)) => Root,
-						(AltShiftSpace, Key(56, Up, _)) => Root,
-						(AltShiftSpace, Key(57, Up, _)) => AltShift,
+            (AltShiftSpace, Key(42, Up, _)) => Root,
+            (AltShiftSpace, Key(56, Up, _)) => Root,
+            (AltShiftSpace, Key(57, Up, _)) => AltShift,
 
-						(AltShiftJ, Key(42, Up, _)) => Root,
-						(AltShiftJ, Key(56, Up, _)) => Root,
-						(AltShiftJ, Key(36, Up, _)) => AltShift,
+            (AltShiftJ, Key(42, Up, _)) => Root,
+            (AltShiftJ, Key(56, Up, _)) => Root,
+            (AltShiftJ, Key(36, Up, _)) => AltShift,
 
-						(AltShiftK, Key(42, Up, _)) => Root,
-						(AltShiftK, Key(56, Up, _)) => Root,
-						(AltShiftK, Key(37, Up, _)) => AltShift,
+            (AltShiftK, Key(42, Up, _)) => Root,
+            (AltShiftK, Key(56, Up, _)) => Root,
+            (AltShiftK, Key(37, Up, _)) => AltShift,
 
-						_ => current_mode
-				};
+            _ => current_mode
+        };
 
-				if next_mode != self.mode {
-						println!("{:?}", next_mode);
-						self.mode = next_mode;
-				}
+        if next_mode != self.mode {
+            println!("{:?}", next_mode);
+            self.mode = next_mode;
+        }
 
-				match (self.mode, &update) {
-						(AltShiftJ, Key(36, Down, _)) => {
-								println!("DOWN!")
-						},
-						(AltShiftK, Key(37, Down, _)) => {
-								self.buff.push_back(Key(103, Down, None));
-								
-								println!("UP!")
-						},
-						(AltShiftSpace, Key(57, Down, _)) => {
-								self.buff.push_back(Key(28, Down, None));
-								println!("RETURN!")
-						},
+        match (self.mode, &update) {
+            (AltShiftJ, Key(36, Down, _)) => {
+                println!("DOWN!")
+            },
+            (AltShiftK, Key(37, Down, _)) => {
+                self.buff.push_back(Key(103, Down, None));
+                
+                println!("UP!")
+            },
+            (AltShiftSpace, Key(57, Down, _)) => {
+                self.buff.push_back(Key(28, Down, None));
+                println!("RETURN!")
+            },
 
-						_ => {}
-				}
-				
+            _ => {}
+        }
+        
 
-				if let Key(_, _, raw) = &update {
-						match raw {
-								Some(_) => self.buff.push_back(update),
-								None => {}
-						}
-				}
+        if let Key(_, _, raw) = &update {
+            match raw {
+                Some(_) => self.buff.push_back(update),
+                None => {}
+            }
+        }
 
-				// let keys = self.keys;
+        // let keys = self.keys;
 
-				// if keys.get(42) && keys.get(56) && keys.get(57) {
+        // if keys.get(42) && keys.get(56) && keys.get(57) {
 
-				// 		self.buff.push_back(Key(28, Down, None));
-				// 		self.buff.push_back(Key(28, Up, None));
-				// }
+        //    self.buff.push_back(Key(28, Down, None));
+        //    self.buff.push_back(Key(28, Up, None));
+        // }
 
-				(0, self.buff.drain(..))
-		}
+        (0, self.buff.drain(..))
+    }
 }
 
 #[derive(PartialEq, Copy, Clone, Debug)]
 enum Mode {
-		Root,
-		Shift,
-		Alt,
-		AltShift,
-		AltShiftSpace,
-		AltShiftJ,
-		AltShiftK
+    Root,
+    Shift,
+    Alt,
+    AltShift,
+    AltShiftSpace,
+    AltShiftJ,
+    AltShiftK
 }
 
 
--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -3,41 +3,57 @@ extern crate winapi;
 
 use std::io::Error;
 use std::ptr::null_mut;
-use winapi::shared::minwindef::{LPARAM, LRESULT, WPARAM};
+use winapi::shared::minwindef::{LPARAM, LRESULT, WPARAM, UINT};
 use winapi::um::winuser::{
     CallNextHookEx, DispatchMessageW, GetMessageW, SetWindowsHookExW, TranslateMessage,
-    UnhookWindowsHookEx, WH_KEYBOARD_LL, MSG, WM_QUIT, KBDLLHOOKSTRUCT, HC_ACTION
+    UnhookWindowsHookEx, WH_KEYBOARD_LL, MSG, WM_QUIT, KBDLLHOOKSTRUCT, HC_ACTION, WM_KEYDOWN, WM_KEYUP
 };
+use std::cell::RefCell;
+use crate::Handler;
+use crate::{Update::*,Movement::*};
 
-unsafe extern "system" fn key_handler(code: i32, wParam: WPARAM, lParam: LPARAM) -> LRESULT {
-    if code == HC_ACTION {
-      let message = wParam as u32;
-      let kb = lParam as *const KBDLLHOOKSTRUCT;
-      let flags = (*kb).flags;
-      let scanCode = (*kb).scanCode;
-      let extra = (*kb).dwExtraInfo;
+type KeyTup = (i32, WPARAM, LPARAM);
 
-      // if scanCode == 91 {
-      //     println!("yum yum!");
-      //     return 1;
-      // }
-
-      println!("kb {} {} {}", scanCode, flags, extra);
+unsafe extern "system" fn key_hook(code: i32, wParam: WPARAM, lParam: LPARAM) -> LRESULT {
+    if code < 0 {
+        return CallNextHookEx(null_mut(), code, wParam, lParam);
     }
+    
+    HANDLER.with(|h| {
+        if let Some(handler) = (*h).borrow_mut().as_mut() {
+            let kb = lParam as *const KBDLLHOOKSTRUCT;
+            let flags = (*kb).flags;
+            let scanCode = (*kb).scanCode;
+            let extra = (*kb).dwExtraInfo;
 
-    CallNextHookEx(null_mut(), code, wParam, lParam)
+            // println!("kb {} {} {}", scanCode, flags, extra);
+
+            let upDown = if flags & 0x80 == 0x80 { Up } else { Down };
+            let key = Key(scanCode as u16, upDown, Some((code, wParam, lParam)));
+
+            let (_next, drain) = handler.handle(key);
+        }
+    });
+
+    1
 }
 
-use crate::Handler;
+thread_local! {
+    static HANDLER: RefCell<Option<Handler<KeyTup>>> = RefCell::new(None);
+}
 
-pub fn run<TState>(_state: &mut TState, _handler: Handler<TState, ()>) -> Result<(), Error> {
-    let _h = unsafe { SetWindowsHookExW(WH_KEYBOARD_LL, Some(key_handler), null_mut(), 0) };
+pub fn run<'a>(create_handler: fn() -> Handler<KeyTup>) -> Result<(), Error> {
+    HANDLER.with(|h| {
+        *h.borrow_mut() = Some(create_handler());
+    });
+
+    let _h = unsafe { SetWindowsHookExW(WH_KEYBOARD_LL, Some(key_hook), null_mut(), 0) };
     if _h.is_null() {
         return Err(Error::last_os_error());
     }
 
     unsafe {
-        let mut msg: MSG = std::mem::uninitialized();
+        let mut msg: MSG = std::mem::MaybeUninit::uninit().assume_init();
         let mut i = 0;
 
         while GetMessageW(&mut msg, null_mut(), 0, 0) > 0 {
