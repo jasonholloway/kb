e25bf98d69b2757574de61de98c9ac26534bf6f1
Jason Holloway <miceeatnicerice@hotmail.com>
2021-03-28 13:15:44 +0100

    runner buffers

--- src/machines.rs
+++ src/machines.rs
@@ -21,38 +21,36 @@ trait Machine<TRaw> {
 
 
 struct Runner<TRaw> {
-    input_buff: VecDeque<Event<TRaw>>,
-    machines: Vec<(Box<dyn Machine<TRaw>>, VecDeque<Event<TRaw>>)>
+    machines: Vec<(Box<dyn Machine<TRaw>>, VecDeque<Event<TRaw>>)>,
+    buff1: VecDeque<Event<TRaw>>,
+    buff2: VecDeque<Event<TRaw>>
 }
 
 impl<'a, TRaw> Runner<TRaw> {
 
     fn new(machines: Vec<(Box<dyn Machine<TRaw>>, VecDeque<Event<TRaw>>)>) -> Runner<TRaw> {
         Runner {
-            input_buff: VecDeque::new(),
-            machines
+            machines,
+            buff1: VecDeque::new(),
+            buff2: VecDeque::new()
         }
     }
     
     fn run(&mut self, ev: Event<TRaw>, sink: &mut Sink<TRaw>) -> () {
-        let mut buff = &mut self.input_buff;
+        let mut buff1 = &mut self.buff1;
+        let mut buff2 = &mut self.buff2;
 
-        buff.push_back(ev);
+        buff1.push_back(ev);
         
-        for (m, buff2) in self.machines.iter_mut() {
-
-            for e in buff.drain(0..) {
-                m.run(e, buff2);
-            }
-
+        for (m, _) in self.machines.iter_mut() {
+            buff2.extend(buff1.drain(0..));
+            
             for e in buff2.drain(0..) {
-                buff.push_back(e);
+                m.run(e, buff1);
             }
         }
 
-        for e in buff.drain(0..) {
-            sink.push_back(e);
-        }
+        sink.extend(buff1.drain(0..));
     }
 }
 
@@ -72,24 +70,29 @@ mod machines_tests {
             }
         }
     }
-
     
 
     #[test]
     fn machines_run_in_sequence() {
         let mut m1 = TestMachine { count: 3 };
         let mut m2 = TestMachine { count: 4 };
+        let mut m3 = TestMachine { count: 5 };
+        let mut m4 = TestMachine { count: 6 };
+        let mut m5 = TestMachine { count: 7 };
 
         let mut runner = Runner::new(vec!(
             (Box::from(m1), VecDeque::new()),
-            (Box::from(m2), VecDeque::new())
+            (Box::from(m2), VecDeque::new()),
+            (Box::from(m3), VecDeque::new()),
+            (Box::from(m4), VecDeque::new()),
+            (Box::from(m5), VecDeque::new())
         ));
 
         let mut sink = Sink::new();
         runner.run(Event::Empty, &mut sink);
         runner.run(Event::Empty, &mut sink);
         
-        assert_eq!(sink.len(), 24)
+        assert_eq!(sink.len(), 2 * 3 * 4 * 5 * 6 * 7)
     }
 
 }
