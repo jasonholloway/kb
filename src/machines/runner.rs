8c497677cfadd514654c9811dcbe013b11f6969f
Jason Holloway <miceeatnicerice@hotmail.com>
2021-04-23 19:05:42 +0100

    machines percolate happily

--- src/machines/runner.rs
+++ src/machines/runner.rs
@@ -11,7 +11,8 @@ pub enum Ev<TUp> {
 
 pub struct Runner<TUp>
 {
-    active: Vec<RunRef<TUp>>,
+    pending: VecDeque<RunRef<TUp>>,
+    seen: VecDeque<RunRef<TUp>>,
     buff1: VecDeque<Ev<TUp>>,
     buff2: VecDeque<Ev<TUp>>,
     buff3: VecDeque<Ev<TUp>>,
@@ -21,7 +22,8 @@ impl<T> Runner<T>
 {
     pub fn new(active: Vec<RunRef<T>>) -> Runner<T> {
         Runner {
-            active,
+            pending: VecDeque::from(active),
+            seen: VecDeque::new(),
             buff1: VecDeque::new(),
             buff2: VecDeque::new(),
             buff3: VecDeque::new(),
@@ -38,26 +40,45 @@ where
         let mut buff2 = &mut self.buff2;
         let mut buff3 = &mut self.buff3;
 
+        let mut pending = &mut self.pending;
+        let mut seen = &mut self.seen;
+
+        if pending.is_empty() {
+            return;
+        }
+
         buff1.push_back(ev);
 
-        for m in self.active.iter_mut() {
-            for e in buff1.drain(0..) {
+        while let Some(mut m) = pending.pop_front() {
 
-                m.inner.run(e, buff2);
+            let mut requeue = true;
+
+            for e1 in buff1.drain(0..) {
+                m.inner.run(e1, buff2);
 
                 for e2 in buff2.drain(0..) {
-
                     match e2 {
-                        Ev::Ev(_) => buff3.push_back(e2),
-                        Ev::Spawn(_) => (),
-                        Ev::Die => ()
+                        Ev::Ev(_) => {
+                            buff3.push_back(e2);
+                        },
+                        Ev::Spawn(m2) => {
+                            pending.push_front(m2)
+                        },
+                        Ev::Die => {
+                            requeue = false;
+                        }
                     }
                 }
             }
 
             buff1.extend(buff3.drain(0..));
+
+            if requeue {
+                seen.push_back(m);
+            }
         }
 
+        pending.extend(seen.drain(0..));
         sink.extend(buff1.drain(0..));
     }
 }
--- src/machines/runner_test.rs
+++ src/machines/runner_test.rs
@@ -2,14 +2,26 @@ use super::*;
 use runner::{Runner,Ev};
 
 #[test]
-fn machines_run_in_sequence() {
+fn percolates() {
     let mut runner = Runner::new(
         vec![
-            RunRef::new("1", TestMachine { count: 3 }),
-            RunRef::new("2", TestMachine { count: 4 }),
-            RunRef::new("3", TestMachine { count: 5 }),
-            RunRef::new("4", TestMachine { count: 6 }),
-            RunRef::new("5", TestMachine { count: 7 }),
+            RunRef::new("2", TestMachine { count: 2 }),
+            RunRef::new("3", TestMachine { count: 3 }),
+        ]);
+
+    let mut sink = VecDeque::new();
+
+    runner.run(Ev(()), &mut sink);
+
+    assert_eq!(sink.len(), 2 * 3)
+}
+
+#[test]
+fn accumulates_in_sink_buffer() {
+    let mut runner = Runner::new(
+        vec![
+            RunRef::new("2", TestMachine { count: 2 }),
+            RunRef::new("3", TestMachine { count: 3 }),
         ]);
 
     let mut sink = VecDeque::new();
@@ -17,61 +29,106 @@ fn machines_run_in_sequence() {
     runner.run(Ev(()), &mut sink);
     runner.run(Ev(()), &mut sink);
 
-    assert_eq!(sink.len(), 2 * 3 * 4 * 5 * 6 * 7)
+    assert_eq!(sink.len(), (2 * 3) + (2 * 3))
 }
 
 #[test]
-fn empty_events_passed_through() {
+fn empty_set_opaque() {
     let mut runner = Runner::new(vec![]);
 
     let mut sink = VecDeque::new();
 
     runner.run(Ev(()), &mut sink);
+    runner.run(Ev(()), &mut sink);
+    runner.run(Ev(()), &mut sink);
 
-    assert_eq!(sink.len(), 1)
+    assert_eq!(sink.len(), 0)
 }
 
 #[test]
-fn machines_add_to_working_set() {
+fn spawns_machines() {
     let mut runner = Runner::new(
         vec![
-            RunRef::new("alf", Alf {}),
+            RunRef::new("alf", Redoubler { i: 0, depth: 2 }),
+        ]);
+
+    let mut sink = VecDeque::new();
+
+    runner.run(Ev::Ev(3), &mut sink);
+
+    assert_eq!(sink.len(), 16)
+}
+
+#[test]
+fn spawns_machines_2() {
+    let mut runner = Runner::new(
+        vec![
+            RunRef::new("baz", Redoubler { i: 10, depth: 1 }),
+            RunRef::new("maz", Redoubler { i: 20, depth: 1 }),
+        ]);
+
+    let mut sink = VecDeque::new();
+
+    runner.run(Ev::Ev(10), &mut sink);
+
+    assert_eq!(sink.len(), 128)
+}
+
+#[test]
+fn machines_die_too() {
+    let mut runner = Runner::new(
+        vec![
+            RunRef::new("Morris", Mayfly { life: 3 }),
         ]);
 
     let mut sink = VecDeque::new();
 
     runner.run(Ev::Ev(1), &mut sink);
-    runner.run(Ev::Ev(1), &mut sink);
-    runner.run(Ev::Ev(1), &mut sink);
+    runner.run(Ev::Ev(2), &mut sink);
+    runner.run(Ev::Ev(3), &mut sink);
+    runner.run(Ev::Ev(4), &mut sink);
+    runner.run(Ev::Ev(5), &mut sink);
 
-    assert_eq!(sink.len(), 12)
+    dbg!(&sink);
+
+    assert_eq!(sink.len(), 3)
 }
 
-
-
-struct Alf {
+struct Mayfly {
+    life: u8,
 }
 
-impl Runnable<u8> for Alf {
+impl Runnable<u8> for Mayfly {
     fn run(&mut self, ev: Ev<u8>, sink: &mut Sink<Ev<u8>>) {
-        sink.push_back(ev);
-        sink.push_back(Ev::Ev(2));
-        sink.push_back(Ev::Spawn(RunRef { tag: "baz", inner: Box::new(Baz {}) }))
+        if self.life > 0 {
+            println!("reemitting");
+            sink.push_back(ev);
+            self.life -= 1;
+        }
+        else{
+            sink.push_back(Ev::Die {});
+        }
     }
 }
 
 
-struct Baz {
+struct Redoubler {
+    i: u8,
+    depth: u8
 }
 
-impl Runnable<u8> for Baz {
+impl Runnable<u8> for Redoubler {
     fn run(&mut self, ev: Ev<u8>, sink: &mut Sink<Ev<u8>>) {
+        println!("D{:?} I{:?} E{:?}", self.depth, self.i, &ev);
+
         sink.push_back(ev);
-        sink.push_back(Ev::Ev(3));
-        sink.push_back(Ev::Ev(4));
+        sink.push_back(Ev::Ev(self.depth));
+
+        if self.depth > 0 {
+          sink.push_back(Ev::Spawn(RunRef { tag: "baz", inner: Box::new(Redoubler { i: self.i + 1, depth: self.depth - 1 }) }))
+        }
     }
 }
-
 
 
 
@@ -81,8 +138,10 @@ struct TestMachine {
 
 impl Runnable<()> for TestMachine {
     fn run(&mut self, ev: Ev<()>, sink: &mut Sink<Ev<()>>) {
+
         for i in 0..self.count {
-            sink.extend(Some(Ev(())));
+            dbg!(i);
+            sink.push_back(Ev(()));
         }
     }
 }
